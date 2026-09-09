402643c89e5697410a370091f84b518cf63fb9c5
Ankit <ankit9667891094@gmail.com>
2026-09-09 16:44:27 +0530

    Refactor sendMsg function and add theme toggle

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function sendMsg(e){
-e.preventDefault();
-alert("Message Sent Successfully 🚀");
+// Contact Form
+function sendMsg(e) {
+    e.preventDefault();
+    alert("Message Sent Successfully 🚀");
 }
+
+
+// Light / Dark Mode
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeBtn.innerHTML = "☀️ Light";
+    } else {
+        themeBtn.innerHTML = "🌙 Dark";
+    }
+});
